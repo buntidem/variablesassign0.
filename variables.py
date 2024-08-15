27ddaee5c90acff1515b5514beebea39d88b2628
buntidem00 <buntidem001@warren.k12.in.us>
2024-08-15 08:44:44 -0400

    intitial commit

---
+++ variables.py
@@ -0,0 +1,8 @@
+num_apples = 20
+num_oranges = 15
+print("Number of apples :",num_apples)
+print("numbers of oranges:", num_oranges)
+num_apples = 0
+num_oranges = 0
+print("Number of apples :",num_apples)
+print("numbers of oranges:", num_oranges)
